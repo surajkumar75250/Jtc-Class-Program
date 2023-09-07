b89befe3588da3861ccec6f4e0a47208d65ab647
surajkumar75250 <68344281+surajkumar75250@users.noreply.github.com>
2023-09-07 14:41:41 +0530

    Program 2.2

---
+++ Jtc6.java
@@ -0,0 +1,32 @@
+class Hello{
+	int a;
+	int ab;
+	//int 1ab;
+	int a1b;
+	int a_b;
+	int ab_;
+	int _ab;
+	int a$b;
+	//int _;
+	int _$;
+	//int ab*;
+	//int ()_;
+	//int for;
+	//int true;
+	int Integer;
+	int INT;
+	int For;
+	void m1(){
+		System.out.println("m1 in Hello");
+		System.out.println(a);
+		System.out.println(Integer);
+		System.out.println(INT);
+		System.out.println(For);
+	}
+}
+public class Jtc6{
+	public static void main(String[] args){
+		Hello h1=new Hello();
+		h1.m1();
+	}
+}
