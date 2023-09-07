836c2c1414e5dcd84acd645cc6f071772d757b8a
surajkumar75250 <68344281+surajkumar75250@users.noreply.github.com>
2023-09-07 20:34:20 +0530

    Add files via upload

---
+++ Jtc2.java
@@ -0,0 +1,13 @@
+class Hello{
+	void m1() {
+		System.out.println("m1 in Hello");
+	}
+}
+public class Jtc2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello h1=new Hello();
+		h1.m1();
+	}
+
+}
